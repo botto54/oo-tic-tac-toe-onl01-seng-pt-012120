5f5c938e16bae70054a9b80a839443e73419b70f
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 22:31:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.any? do |combo|
-      if position_taken?(combo[0])
+      if position_taken?(combo[0]) && @board([0]) = 
   end
   
   def full?
