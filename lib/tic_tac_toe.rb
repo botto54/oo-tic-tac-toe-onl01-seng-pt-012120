1d759f356d752b1a35f385dc9338b74a5bf9c1e8
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 22:51:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,7 +89,7 @@ class TicTacToe
   
   def play
     turn until over?
-    puts winner ? "Congratulations #{winner}!" : "Cat's game!"
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end
   
 end
